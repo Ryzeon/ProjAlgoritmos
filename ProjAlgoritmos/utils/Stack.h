d362b7452d0a1abc7eca9575d9e85db4c81dd08a
Raizeon <alex.ramon2004@gmail.com>
2023-06-17 18:47:37 -0500

    feat: Added queue

--- ProjAlgoritmos/utils/Stack.h
+++ ProjAlgoritmos/utils/Stack.h
@@ -1,1 +1,63 @@
 #pragma once
+
+#include "INode.h"
+
+ namespace utils {
+
+    template<class T>
+    class Stack {
+    private:
+        INode<T> *node;
+        int size;
+
+    public:
+        Stack() {
+            node = nullptr;
+            size = 0;
+        }
+
+        void push(T data) {
+            auto *newNode = new INode<T>(data);
+            if (node == nullptr) {
+                node = newNode;
+            } else {
+                newNode->next = node;
+                node = newNode;
+            }
+            size++;
+        }
+
+        T pop() {
+            if (node == nullptr) {
+                throw "Stack is empty";
+            }
+            INode<T> *temp = node;
+            node = node->next;
+            T data = temp->data;
+            delete temp;
+            size--;
+            return data;
+        }
+
+        T peek() {
+            if (node == nullptr) {
+                throw "Stack is empty";
+            }
+            return node->data;
+        }
+
+        bool isEmpty() {
+            return node == nullptr;
+        }
+
+        void clear() {
+            while (!isEmpty()) {
+                pop();
+            }
+        }
+
+        ~Stack() {
+            clear();
+        }
+    };
+}
