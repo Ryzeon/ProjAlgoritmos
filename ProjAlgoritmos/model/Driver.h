c4e804a3ebb4f63b5ef73f44b3ffe3accd1be042
Raizeon <alex.ramon2004@gmail.com>
2023-06-19 08:29:58 -0500

    added driver

--- ProjAlgoritmos/model/Driver.h
+++ ProjAlgoritmos/model/Driver.h
@@ -1,5 +1,27 @@
 #pragma once
 
-class Driver {
+#include "Client.h"
+
+namespace drive_now_models {
+
+    class CarInfo {
+    public:
+        CarInfo() {
+
+        }
+
+    public:
+        []
+    };
+
+
+	class Driver: public Client {
+	public:
+		string active_service;
+		CarInfo* car_info;
+        
 
 };
+
+
+}
